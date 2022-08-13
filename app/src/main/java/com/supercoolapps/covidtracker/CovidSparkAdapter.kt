a11979cf7b026f6b7c09d57320bdecd96030f4ff
mishra_mridul <mridulmishra2991@gmail.com>
2022-08-13 17:44:42 +0530

    feat: change chart data by radio select

--- app/src/main/java/com/supercoolapps/covidtracker/CovidSparkAdapter.kt
+++ app/src/main/java/com/supercoolapps/covidtracker/CovidSparkAdapter.kt
@@ -1,14 +1,29 @@
 package com.supercoolapps.covidtracker
 
+import android.graphics.RectF
 import com.robinhood.spark.SparkAdapter
 
 class CovidSparkAdapter(private val dailyData: List<CovidData>): SparkAdapter() {
+    var metric = Metric.POSITIVE
+    var daysAgo = TimeScale.MAX
     override fun getCount(): Int  = dailyData.size
 
     override fun getItem(index: Int): Any = dailyData[index]
 
     override fun getY(index: Int): Float {
         val chosenDayData = dailyData[index]
-        return chosenDayData.positiveIncrease.toFloat()
+        return when(metric) {
+            Metric.NEGATIVE -> chosenDayData.negativeIncrease.toFloat()
+            Metric.POSITIVE -> chosenDayData.positiveIncrease.toFloat()
+            Metric.DEATH -> chosenDayData.deathIncrease.toFloat()
+        }
+    }
+
+    override fun getDataBounds(): RectF {
+       val bounds = super.getDataBounds()
+        if (daysAgo != TimeScale.MAX){
+            bounds.left = count - daysAgo.numDays.toFloat()
+        }
+        return bounds
     }
 }
